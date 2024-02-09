63097301f56961584702dc0f09c794f28dd7fdeb
Grahamsley <stoddarg@gmail.com>
2024-02-09 16:38:43 -0500

    Add files via upload
    Changed the MeasureROI.m file into a function so that RunMeasureROI.m can run it in batch.

--- MeasureROI.m
+++ MeasureROI.m
@@ -1,37 +1,62 @@
-% Measure ROI script
-% Description of the function of this script
-% input variables, if any
-% output variables, if any
-clear
+% Measure ROI function
+% Description:
+%   This function places circular ROIs onto images of the Gammex 
+%   Multi-Energy CT Phantom in order to measure mean HU, min HU, max Hu, 
+%   and standard deviation of the ROI. Before doing that, it measures the 
+%   locations of the 4 centering BBs in the phantom and calculates a 
+%   rotation and offset, then shifts the ROIs so they are as close to the 
+%   center of the rod locations as possible. 
+%   Before placing and measuring the ROIs, the code produces 2 png images
+%   of the ROIs overlaid on the images to allow the user to verify they are
+%   being placed accurately.
+%   This code then reads in an effective Z map and places ROIs in the same
+%   locations as the original images and measures the same quantities. 
+%   The code then finally reads in an electron density map and does the
+%   same. 
+
+% Input variables:
+%   DirPath = string
+%   The path to the folder which contains 3 folders, one for the VMI DICOM
+%   files, one for the effective Z DICOM files, and one for the electron
+%   density DICOM files.
+
+% output variables:
+%   None
+
+function MeasureROI(DirPath)
+% clear
 
 % User Defined Settings
-% SliceThickness % we find this based on the read in DICOM volume
-
-% defining variables
 sliceRegions = [41:50,59:68,77:86,96:105,114:123]; % Ben's slice regions
 % sliceRegions = [77:83]; % Aroon's slice regions
 numROIs = 9; % Ben's ROIs to be placed
 % numROIs = 8; Aroon's ROIs to be placed
-imCenter = 256;
+% imCenter = 256;
 ROIRadius = 28.5 * 0.6; % in mm
 BBLocations = zeros(4,2);
 dist2BB = 100; % in mm
 dist2ROIperiph = 75; % in mm
 dist2ROIperiphDiag = dist2ROIperiph / sqrt(2);
-ROICenterLoc = zeros(9,2);
 
+ROICenterLoc = zeros(9,2);
 saveVar = zeros(numel(sliceRegions),numROIs,4);
 
 %% read in the 4D DICOM volume
 % ask the user for the location of the DICOM files to read in
 % Note to the user: only place 1 series of DICOM files into the folder
-i = 1;
-while i <= 1
-    DirPath = uigetdir(); % pop up a windows explorer box
-    i = i + 1;
-end
+% i = 1;
+% while i <= 1
+%     DirPath = uigetdir(); % pop up a windows explorer box
+%     i = i + 1;
+% end
 
-[testVol, spt, dims] = dicomreadVolume(DirPath);
+% now create the foldernames for each of the VMI, effZ, pe DICOM files
+VMIfolder = strcat(DirPath, '\VMI');
+effectiveZfolder = strcat(DirPath, '\EffectiveZ');
+ElectronDensityfolder = strcat(DirPath, '\ElectronDensity');
+
+% read in the VMI DICOM files
+[testVol, spt] = dicomreadVolume(VMIfolder);
 testVolSq = squeeze(testVol);
 
 %% Identify the orientation BB's in the central slice of the phantom
@@ -104,19 +129,6 @@ pfit2 = polyfit([BBLocations(1,1),BBLocations(3,1)],[BBLocations(1,2),BBLocation
 xCenter = (pfit1(2)-pfit2(2))/(pfit2(1)-pfit1(1));
 yCenter = (pfit1(2)*pfit2(1)-pfit1(1)*pfit2(2))/(pfit2(1)-pfit1(1));
 
-% create data points for the fits
-% xF = linspace(0, 512,1000);
-% yF = polyval(pfit1,xF);
-% yF2 = polyval(pfit2,xF);
-% plot(xF,yF)
-% hold on
-% plot(xF,yF2)
-% [xCenter, yCenter] = polyxpoly(xF,yF,xF,yF2);
-% the difference between this calculated center and the ideal center is how
-% far each ROI needs to be shifted to be centered on the rods
-% xOffset = xCenter - imCenter;
-% yOffset = yCenter - imCenter;
-
 % using the righthand BB, calculate the amount of rotation that is found
 % values in this equation are in pixels
 % angleRot = atan((BBLocations(3,2)-yCenter)/(512-xCenter)); % * 180/pi;
@@ -126,21 +138,6 @@ angleRot = atan((BBLocations(3,2)-yCenter)/(dist2BB)); % * 180/pi;
 rotationMat = [cos(angleRot), sin(angleRot); -sin(angleRot), cos(angleRot)];
 
 %% define regions, distances and centers to ROIs 
-% these locations worked, but now that we're using the shifted and rotated
-% coordinates, they are not exact
-% rod placement: rod 1 at center, 2-9 at periphery with rod 2 at 12 o'clock
-% position
-% coordinates: [+right-left +up-down]
-% ROICenterLoc(1,:)= [imCenter, imCenter];
-% ROICenterLoc(2,:)= [imCenter, imCenter + dist2ROIperiph];
-% ROICenterLoc(3,:)= [imCenter + dist2ROIperiphDiag, imCenter + dist2ROIperiphDiag];
-% ROICenterLoc(4,:)= [imCenter + dist2ROIperiph, imCenter];
-% ROICenterLoc(5,:)= [imCenter + dist2ROIperiphDiag, imCenter + -dist2ROIperiphDiag];
-% ROICenterLoc(6,:)= [imCenter, imCenter + -dist2ROIperiph];
-% ROICenterLoc(7,:)= [imCenter + -dist2ROIperiphDiag, imCenter + -dist2ROIperiphDiag];
-% ROICenterLoc(8,:)= [imCenter + -dist2ROIperiph, imCenter];
-% ROICenterLoc(9,:)= [imCenter + -dist2ROIperiphDiag, imCenter + dist2ROIperiphDiag];
-
 ROICenterLoc(1,:)= [0, 0];
 ROICenterLoc(2,:)= [0, 0 + dist2ROIperiph];
 ROICenterLoc(3,:)= [0 + dist2ROIperiphDiag, 0 + dist2ROIperiphDiag];
@@ -160,105 +157,114 @@ ROICenterLoc(:,1) = ROICenterLoc(:,1) + xCenter;
 ROICenterLoc(:,2) = ROICenterLoc(:,2) + yCenter;
 
 %% ask user to confirm the placement of the ROIs
-% % comment this section once we have confirmed the center is acceptable
-% figure;
-% 
-% % place an ROI
-% slice = squeeze(testVolSq(:,:,sliceRegions(24)));
-% imshow(slice,"DisplayRange",[-150 150])
-% % 
-% ROI1 = drawcircle("Center",ROICenterLoc(1,:),"Radius",ROIRadius);
-% ROI2 = drawcircle("Center",ROICenterLoc(2,:),"Radius",ROIRadius);
-% ROI3 = drawcircle("Center",ROICenterLoc(3,:),"Radius",ROIRadius);
-% ROI4 = drawcircle("Center",ROICenterLoc(4,:),"Radius",ROIRadius);
-% ROI5 = drawcircle("Center",ROICenterLoc(5,:),"Radius",ROIRadius);
-% ROI6 = drawcircle("Center",ROICenterLoc(6,:),"Radius",ROIRadius);
-% ROI7 = drawcircle("Center",ROICenterLoc(7,:),"Radius",ROIRadius);
-% ROI8 = drawcircle("Center",ROICenterLoc(8,:),"Radius",ROIRadius);
-% ROI9 = drawcircle("Center",ROICenterLoc(9,:),"Radius",ROIRadius);
-% 
-% 
-% % present options to the user and have them select one 
-% offset = 1;
-% 
-% while offset ~= 0
-%     answer = questdlg('Is the centering acceptable?', ...
-%         'Measure ROI', ...
-%         'Yes','No','Ice cream, please.');
-% 
-%     switch answer
-%         case 'Yes'
-%             offset = 0;
-%         case 'No'
-%             % ask user what the offset should be
-%             % then replot and ask again
-%         case 'Ice cream, please.'
-%             offset = 0;
-%         otherwise
-%             % handle the user closing the box without making a selection
-%     end
-% 
-% 
-%     if answer == 0 % the user clicked 'cancel' or pressed 'Esc'
-%         MsgText = "Error choosing which series to plot.";
-%         uiwait(msgbox(MsgText, "TCM Plot", "modal"));
-% 
-%         return
-%     end
-% end
+% comment this section once we have confirmed the center is acceptable
+f1 = figure;
 
-%% move the ROI around to each rod and make measurements
-
-% this works
-% slice = squeeze(testVolSq(:,:,sliceRegions(1)));
-% imshow(slice)
-% ROI = drawcircle("Center",ROICenterLoc(1,:),"Radius",ROIRadius);
-
-k = 1;
-ROIcenter = 1;
-
-for i = 1:numel(sliceRegions)
-    % place an ROI
-    slice = squeeze(testVolSq(:,:,sliceRegions(i)));
-    imshow(slice,"DisplayRange",[-150 150])
-    ROI = drawcircle("Center",ROICenterLoc(ROIcenter,:),"Radius",ROIRadius);
-    
-    for j = 1:numROIs
-        % measure average, SD, min, max
-        ROImask = ROI.createMask;
-        ROImean = mean(slice(ROImask));
-        ROIsd = std2(slice(ROImask));
-        ROImin = min(slice(ROImask));
-        ROImax = max(slice(ROImask));
-
-        % save these values to our arrays
-        % going to use a 50 x 9 x 4 array
-        % 50 slices of data with 9 ROIs with 4 different values
-        saveVar(i,j,1) = ROImean;
-        saveVar(i,j,2) = ROIsd;
-        saveVar(i,j,3) = ROImin;
-        saveVar(i,j,4) = ROImax;
-
-        % move the ROI to each location
-        if ROIcenter < 9
-            ROIcenter = ROIcenter + 1;
-            ROI.Center = ROICenterLoc(ROIcenter,:);
-        end
+for d = 1:2
+    if d == 1
+        checkROI = sliceRegions(1);
+    else
+        checkROI = sliceRegions(41);
     end
-    ROIcenter = 1;
+
+    % place an ROI
+    slice = squeeze(testVolSq(:,:,checkROI));
+    imshow(slice,"DisplayRange",[-150 150])
+
+    drawcircle("Center",ROICenterLoc(1,:),"Radius",ROIRadius);
+    drawcircle("Center",ROICenterLoc(2,:),"Radius",ROIRadius);
+    drawcircle("Center",ROICenterLoc(3,:),"Radius",ROIRadius);
+    drawcircle("Center",ROICenterLoc(4,:),"Radius",ROIRadius);
+    drawcircle("Center",ROICenterLoc(5,:),"Radius",ROIRadius);
+    drawcircle("Center",ROICenterLoc(6,:),"Radius",ROIRadius);
+    drawcircle("Center",ROICenterLoc(7,:),"Radius",ROIRadius);
+    drawcircle("Center",ROICenterLoc(8,:),"Radius",ROIRadius);
+    drawcircle("Center",ROICenterLoc(9,:),"Radius",ROIRadius);
+
+    filenamePlacedROIs = strcat(DirPath, '\PlacedROI',int2str(checkROI),'.png');
+    saveas(f1,filenamePlacedROIs);
+
 end
 
-%% export values into excel template
-% eventually we'll have to code this up to accept input containing the
-% location of the DICOM files to read and the location of the folder where
-% the output excel file should be placed
-filename = strcat(DirPath,'\ROIdata.xlsx');
+close(f1);
 
-% change our array to look like our excel template:
-writematrix(saveVar(:,:,1),filename,'Sheet',3,'Range','B2:J51')
-writematrix(saveVar(:,:,2),filename,'Sheet',4,'Range','B2:J51')
-writematrix(saveVar(:,:,3),filename,'Sheet',5,'Range','B2:J51')
-writematrix(saveVar(:,:,4),filename,'Sheet',6,'Range','B2:J51')
+%% move the ROI around to each rod and make measurements
+% run over the VMI first (m = 1), then place the same ROIs on the 
+% effective Z (m = 2) and electron density maps (m = 3)
+for m = 1:3
+    ROIcenter = 1;
+
+    for i = 1:numel(sliceRegions)
+        % place an ROI
+        slice = squeeze(testVolSq(:,:,sliceRegions(i)));
+        % W:400 and C:775 for eff Z
+        % W:4000 and C:1915 for pe
+        imshow(slice,"DisplayRange",[-150 150])
+        ROI = drawcircle("Center",ROICenterLoc(ROIcenter,:),"Radius",ROIRadius);
+
+        for j = 1:numROIs
+            % measure average, SD, min, max
+            ROImask = ROI.createMask;
+            ROImean = mean(slice(ROImask));
+            ROIsd = std2(slice(ROImask));
+            ROImin = min(slice(ROImask));
+            ROImax = max(slice(ROImask));
+
+            % save these values to our arrays
+            % going to use a 50 x 9 x 4 array
+            % 50 slices of data with 9 ROIs with 4 different values
+            saveVar(i,j,1) = ROImean;
+            saveVar(i,j,2) = ROIsd;
+            saveVar(i,j,3) = ROImin;
+            saveVar(i,j,4) = ROImax;
+
+            % move the ROI to each location
+            if ROIcenter < 9
+                ROIcenter = ROIcenter + 1;
+                ROI.Center = ROICenterLoc(ROIcenter,:);
+            end
+        end
+        ROIcenter = 1;
+    end
+
+    currFig = gcf;
+    close(currFig);
+
+    switch m
+        case 1
+            filename = strcat(DirPath,'\ROIdataVMI.xlsx');
+        case 2
+            filename = strcat(DirPath,'\ROIdataEffZ.xlsx');
+        case 3
+            filename = strcat(DirPath,'\ROIdataElecDens.xlsx');
+        otherwise
+            filename = strcat(DirPath,'\ROIdata',int2str(m),'.xlsx');
+    end
+
+    % change our array to look like our excel template:
+    writematrix(saveVar(:,:,1),filename,'Sheet',3,'Range','B2:J51')
+    writematrix(saveVar(:,:,2),filename,'Sheet',4,'Range','B2:J51')
+    writematrix(saveVar(:,:,3),filename,'Sheet',5,'Range','B2:J51')
+    writematrix(saveVar(:,:,4),filename,'Sheet',6,'Range','B2:J51')
+
+    % read in the effective Z DICOM files
+    switch m
+        case 1
+            foldername = effectiveZfolder;
+        case 2
+            foldername = ElectronDensityfolder;
+        case 3
+            break;
+    end
+
+    testVol = dicomreadVolume(foldername);
+    testVolSq = squeeze(testVol);
+
+end
+
+
+
+
 
 
 
